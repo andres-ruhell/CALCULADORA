b809111cc802acce6e95bc96a00e234c6b54dfd7
Andrés Ruiz Hellemann <andresruhell@me.com>
2023-09-25 19:39:01 -0600

    commit recibe un caracter y define si es suma o ...

--- calculadora_git.c
+++ calculadora_git.c
@@ -2,5 +2,27 @@
 
 int main(void){
 
+	char a = 0;
+
+	scanf("%c", &a);
+
+	switch(a){
+
+	case '+':
+		printf("Suma\n");
+		break;
+	case '-':
+        printf("Resta\n");
+break;
+	case '*':
+        printf("Multiplicacion\n");
+break;
+	case '/':
+        printf("División\n");
+break;
+
+	printf("No válido\n");
+}
+
 	return 0;
 }
